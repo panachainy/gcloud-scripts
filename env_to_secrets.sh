4e9f0b3f29ba54b91980f9c3ee70b604647f4374
Panachai <panachai.ny@gmail.com>
2023-01-10 16:38:39 +0700

    Update env_to_secrets.sh

--- env_to_secrets.sh
+++ env_to_secrets.sh
@@ -2,13 +2,29 @@
 
 PROJECT=default-project
 
-# check if --check is provided as an argument
-if [ "$1" == "--r" ]; then
+# check if --run is provided as an argument
+if [ "$1" == "--run" ]; then
     RUN_MODE=true
     shift
 else
     RUN_MODE=false
 fi
+
+# check for project argument
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        --project)
+        PROJECT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        shift # past argument
+        ;;
+    esac
+done
 
 # Read the .env file
 while IFS='=' read -r key value; do
@@ -23,5 +39,4 @@ while IFS='=' read -r key value; do
       echo "printf %s $value | gcloud secrets create $key --data-file=- --replication-policy=automatic --labels=project=$PROJECT"
     fi
     # Create the secret in Google Cloud Secrets Manager
-
-done < .env.example
+done < .env
